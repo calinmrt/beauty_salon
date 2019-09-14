646ac3f64f433cff2dcd4ef54914af738221f51b
Calin Marta <calin.mrt@gmail.com>
2019-09-14 15:28:08 +0300

    first commit

---
+++ src/main/java/com/sda/bobbeautybar/BobbeautybarApplication.java
@@ -0,0 +1,13 @@
+package com.sda.bobbeautybar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobbeautybarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobbeautybarApplication.class, args);
+    }
+
+}
